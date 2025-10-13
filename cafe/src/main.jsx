82576b0bc74cdf53ba7aeb1acd09b9c360b8f007
VeronicaGrisales <verogrisales4@gmail.com>
2025-10-13 18:26:44 -0500

    Ceación de carpetas y enrutamientos, ya funcionando

--- cafe/src/main.jsx
+++ cafe/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Inicio from './paginas/inicio.jsx';   // Página de inicio
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Definir las rutas
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Inicio />,   // otra ruta (opcional)
+  },
+  // {
+  //   path: '/app',
+  //   element: <App />,      //  otra ruta (opcional)
+  // },
+])
+
+// Renderizar la aplicación
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
+
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
---
+++ cafe/src/paginas/Inicio.jsx
@@ -0,0 +1,11 @@
+function Inicio() {
+  return (
+    <div className="inicio">
+      <h1>Bienvenido a Café Nazareth</h1>
+      <p>Disfruta del mejor café artesanal colombiano.</p>
+    </div>
+  );
+}
+
+export default Inicio;
+
